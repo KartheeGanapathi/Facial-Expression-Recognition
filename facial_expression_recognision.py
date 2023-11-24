5bf40f124a4c2248bd8974f77b61434254f81632
Kartheepan G <83823805+KartheeGanapathi@users.noreply.github.com>
2023-11-24 19:33:11 +0530

    Updated facial_expression_recognision.py with inclusion of facial recognition

--- facial_expression_recognision.py
+++ facial_expression_recognision.py
@@ -1,57 +1,70 @@
+import io
 import numpy as np
-import matplotlib.pyplot as plt
-
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import load_img
-
 from PIL import Image
 import cv2
 import streamlit as st
 
-st.title("Facial Expression Recognision App")
-st.write("Upload an image to recogonize their expression")
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
+# Load the trained model
 loadedModelTF = tf.keras.models.load_model('facialExpressionRecognisionTF.h5')
 
-def featureExtractionSingle(image):
+def feature_extraction_single(image):
     features = []
-    img = load_img(image, grayscale=True)
+    try:
+        img = image.convert('L')
+    except:
+        img = load_img(image, grayscale=True)
     img = np.array(img)
     img = cv2.resize(img, (48, 48))
     features.append(img)
     features = np.array(features)
     return features, img
 
-col1, col2 = st.columns(2)
+def emotion_prediction(image):
+    new_img_features, new_img = feature_extraction_single(image)
+    pred = loadedModelTF.predict(new_img_features.reshape(1, 48, 48, 1))
 
-with col1:
-    uploaded_image = st.file_uploader("Upload an image below", type=["jpg", "jpeg", "png"])
+    emotion_labels = ['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
+    prediction_label = emotion_labels[np.argmax(pred)]
 
-with col2:
-    picture = st.camera_input("Take a picture")
-    if picture :
-        uploaded_image = picture
+    return prediction_label
 
-try:
-    newImgFeatures, newImg = featureExtractionSingle(uploaded_image)
-    pred = loadedModelTF.predict(newImgFeatures.reshape(1, 48, 48, 1))
-    if pred[0][0] == 1:
-        prediction_label = 'angry'
-    elif pred[0][1] == 1:
-        prediction_label = 'disgust'
-    elif pred[0][2] == 1:
-        prediction_label = 'fear'
-    elif pred[0][3] == 1:
-        prediction_label = 'happy'
-    elif pred[0][4] == 1:
-        prediction_label = 'neutral'
-    elif pred[0][5] == 1:
-        prediction_label = 'sad'
-    else:
-        prediction_label = 'surprise'
+def streamlitInterface():
+    st.title("Facial Expression Recognition App")
+    st.write("Upload an image to recognize the facial expression")
 
-    image = Image.open(uploaded_image)
-    st.image(image, caption="Uploaded Image")
-    st.write("\n\nPredicted Output:", prediction_label)
-except:
-    pass
+    col1, col2 = st.columns(2)
+
+    with col1:
+        uploaded_image = st.file_uploader("Upload an image below", type=["jpg", "jpeg", "png"])
+
+    with col2:
+        picture = st.camera_input("Take a picture")
+        if picture:
+            uploaded_image = picture
+
+    if uploaded_image is not None:
+        image_data = uploaded_image.read()
+        uploaded_pil_image = Image.open(io.BytesIO(image_data))
+
+        gray = load_img(uploaded_image, grayscale=True)
+        gray = np.array(gray)
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+        if len(faces) > 0:
+            emotions = [emotion_prediction(uploaded_pil_image.crop((x, y, x + w, y + h))) for x, y, w, h in faces]
+
+            for (x, y, w, h), prediction_label in zip(faces, emotions):
+                short_image = uploaded_pil_image.crop((x, y, x + w, y + h))
+                st.image(short_image)
+                st.write('\n\nPredicted Emotion:', prediction_label)
+        else:
+            pred = emotion_prediction(uploaded_image)
+            st.image(uploaded_image)
+            st.write('\n\nPredicted Emotion:', pred)
+
+
+streamlitInterface()
